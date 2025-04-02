b70b21a1e3c45d018a98be92030c00b5f66bc054
Giulio <86263020+maikuITA@users.noreply.github.com>
2025-04-02 18:40:51 +0200

    boh

--- Progetto/Smarty/templates_c/0c3381c375370c3f0376787f46096c2c7456a2ad_0.file_home.tpl.php
+++ Progetto/Smarty/templates_c/0c3381c375370c3f0376787f46096c2c7456a2ad_0.file_home.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.3, created on 2025-04-02 18:37:40
+/* Smarty version 5.4.3, created on 2025-04-02 18:39:41
   from 'file:home.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.3',
-  'unifunc' => 'content_67ed67d4547706_35848648',
+  'unifunc' => 'content_67ed684d6fe1f9_02372305',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0c3381c375370c3f0376787f46096c2c7456a2ad' => 
     array (
       0 => 'home.tpl',
-      1 => 1743001645,
+      1 => 1743611979,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_67ed67d4547706_35848648 (\Smarty\Template $_smarty_tpl) {
+function content_67ed684d6fe1f9_02372305 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/opt/lampp/htdocs/DigitalPlot/Progetto/Smarty/templates';
 ?><!DOCTYPE html>
 <html lang="it">
@@ -39,8 +39,8 @@ $_smarty_current_dir = '/opt/lampp/htdocs/DigitalPlot/Progetto/Smarty/templates'
     <div class="left-section">
       <div class="logo">OnlyFans</div>
       <nav class="main-nav">
-        <a href="/">HOMEPAGE</a>
-        <a href="/prodotti">PRODOTTI</a>
+        <a href="/DigitalPlot/">HOMEPAGE</a>
+        <a href="/DigitalPlot/prodotti">PRODOTTI</a>
       </nav>
     </div>
     <div class="right-section">
