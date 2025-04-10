89ad832af0c3898178c41bc404b0382a7326fb27
3ndryw <andreacostantini03@gmail.com>
2025-04-10 17:33:32 +0200

    CreazioeClasseArticolo

---
+++ Progetto/Entity/EArticolo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Entity;
+
+class EArticolo{
+    private int $id;
+    private string $titolo = "";
+    private string $descrizione;
+    private string $genere;
+    private string $categoria;
+}
+
+
+?>
