8835993ad13d2c8915bdac32c3ec6a2b3de1579a
3ndryw <andreacostantini03@gmail.com>
2025-04-10 18:31:00 +0200

    Update EAcquisto.php

--- Progetto/Entity/EAcquisto.php
+++ Progetto/Entity/EAcquisto.php
@@ -5,15 +5,19 @@ namespace Entity;
 class EAcquisto{
     private int $codice;
     private EData $dataAcquisto;
+    private EAbbonamento $abbonamento;
+    private ESconto $sconto;    
     private float $subTotale;
 
 
-    public function __construct(int $codice, EData $dataAcquisto, float $subTotale) {
+    public function __construct(int $codice, EData $dataAcquisto, EAbbonamento $abbonamento, ESconto $sconto = new ESconto( "", 0)) {
         $this->codice = $codice;
         $this->dataAcquisto = $dataAcquisto;
-        $this->subTotale = $subTotale;
+        $this->abbonamento = $abbonamento;
+        $this->sconto = $sconto;
+        $this->subTotale = $abbonamento->getImporto() - $sconto->getImporto();
     }
-
+    
     // Set methods
     public function setCodice(int $codice)
     {
@@ -23,9 +27,13 @@ class EAcquisto{
     {
         $this->dataAcquisto = $dataAcquisto;
     }
-    public function setSubTotale($subTotale)
+    public function setAbbonamento(EAbbonamento $abbonamento)
     {
-        $this->subTotale = $subTotale;
+        $this->abbonamento = $abbonamento;
+    }
+    public function setSconto(ESconto $sconto)
+    {
+        $this->sconto = $sconto;
     }
     // Get methods
     public function getCodice(){
@@ -36,6 +44,12 @@ class EAcquisto{
     }
     public function getSubTotale(){
         return $this->subTotale;
+    }
+    public function getAbbonamento(){
+        return $this->abbonamento;
+    }
+    public function getSconto(){
+        return $this->sconto;
     }
 
 }
