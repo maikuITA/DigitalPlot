c9b16b6402c92d31668f17e42b30e37babe9dc86
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-04-10 17:40:40 +0200

    Creazione CartaDI CRedito

---
+++ Progetto/Entity/CartaDiCredito.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Entity;
+
+class CartaDiCredito{
+    public int $numeroCarta;
+    public string $nome;
+    public string $cognome;
+    public string $dataScadenza;
+
+
+    // Set methods
+    public function setNumeroCarta($numeroCarta)
+    {
+        $this->numeroCarta = $numeroCarta;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome;
+    }
+
+    public function setDataScadenza($dataScadenza)
+    {
+        $this->dataScadenza = $dataScadenza;
+    }
+
+    // Get methods
+    public function getNumeroCarta(){
+        return $this->numeroCarta;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getCognome(){
+        return $this->cognome;
+    }
+
+    public function getDataScadenza(){
+        return $this->dataScadenza;
+    }
+
+}
