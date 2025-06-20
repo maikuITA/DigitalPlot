458d17bc634680aba639f0e3f195884ee84b48d1
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-20 17:08:25 +0200

    Update VSearch.php

--- Progetto/View/VSearch.php
+++ Progetto/View/VSearch.php
@@ -1,5 +1,20 @@
 <?php
 
+class VSearch {
+    
+    /**
+     * Method to display the search results
+     * @param array $articles
+     * @return void
+     */
+    public static function displaySearchResults(array $articles): void {
+       $smarty = StartSmarty::configuration();
+       ULogSys::toLog("Display -> ricerca.tpl");
+       $smarty->assign('articles', $articles);
+       $smarty->display('ricerca.tpl');
+    }
+
+}
 
 
 ?>
