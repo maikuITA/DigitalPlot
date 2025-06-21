267be4562d1a491de6c4bc8e0a7a79418e04cec9
3ndryw <andreacostantini03@gmail.com>
2025-06-21 12:26:20 +0200

    update LeggiLog

--- Progetto/Utility/ULeggiErrori.php
+++ Progetto/Utility/ULeggiErrori.php
@@ -3,26 +3,14 @@
     $path = __DIR__ . '/Logs/errors.log';
     // DEBUG: echo $logs;
 
-    // Verifica se il file esiste
     if (file_exists($path)) {
-        // Apri il file in modalità lettura ('r')
-        $file = fopen($path, 'r');
-
-        // Verifica se l'apertura del file ha avuto successo
-        if ($file) {
-            // Leggi il file riga per riga
-            while (($riga = fgets($file)) !== false) {
-                // Stampa la riga
-                echo htmlspecialchars($riga) . '<br>'; // htmlspecialchars protegge da XSS
-            }
-
-            // Chiudi il file
-            fclose($file);
-        } else {
-            echo 'Errore nell\'apertura del file.';
-        }
+        /**
+         * file_get_contents reads the entire file into a string. Gli accapo diventano \n
+         * and then nl2br converts them to <br> tags for HTML display.
+         */
+        echo nl2br(file_get_contents($path));
     } else {
-        echo 'Il file non esiste.';
+        echo "File di log non trovato: $path";
     }
 
 ?>
