b5286231cb35ed88794a96188cd5f33413d28386
3ndryw <andreacostantini03@gmail.com>
2025-06-21 11:29:36 +0200

    update V/C Article

--- Progetto/Controller/CArticle.php
+++ Progetto/Controller/CArticle.php
@@ -2,17 +2,27 @@
 
 class CArticle{
 
-    public static function showArticle():void{
+    public static function showArticle(?int $idArticolo):void{
+        if($idArticolo === null || $idArticolo <= 0){
+            header('Location: https://digitalplot.altervista.org/error');
+            exit();
+        }
         if(CUser::isLogged()){
+            $article = FPersistentManager::getInstance()->retriveObjById(EArticle::class, $idArticolo);
+            if(!isset($article)){
+                header('Location: https://digitalplot.altervista.org/error');
+                exit();
+            }
             $user = FPersistentManager::getInstance()->retriveObjById(EUser::class, USession::getSessionElement('user'));
             if(CUser::isSubbed()){
-                VArticle::showArticle(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), true);
+                VArticle::showArticle(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), true, $article, $article->getWriter());
             }
             else{
-                VArticle::showArticle(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), false);
+                VArticle::showArticle(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), false, $article, $article->getWriter());
             }
         }else{
             header('Location: https://digitalplot.altervista.org/auth');
+            exit();
         }
     }
 }
--- Progetto/View/VArticle.php
+++ Progetto/View/VArticle.php
@@ -2,13 +2,15 @@
 
 class VArticle{
 
-    public static function showArticle(bool $isLogged = false, int $plotPoints = 0 , $proPic = null , bool $isAbbonato = false ): void {
+    public static function showArticle(bool $isLogged = false, int $plotPoints = 0 , $proPic = null , bool $isAbbonato = false, EArticle $article, EWriter $writer): void {
         $smarty = StartSmarty::configuration();
         ULogSys::toLog("Display -> lettura.tpl");
         $smarty->assign('isLogged', $isLogged);
         $smarty->assign('isAbbonato', $isAbbonato);
         $smarty->assign('plotPoints', $plotPoints);
         $smarty->assign('proPic', $proPic);
+        $smarty->assign('article', $article);
+        $smarty->assign('writer', $writer);
         $smarty->display('lettura.tpl');
     }
 }
