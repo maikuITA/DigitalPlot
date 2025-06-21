49ae1f3bde35ca7123ab6404879c6e6cddf36614
3ndryw <andreacostantini03@gmail.com>
2025-06-21 12:07:47 +0200

    Update log.js

--- Progetto/Smarty/js/log.js
+++ Progetto/Smarty/js/log.js
@@ -1,28 +1,50 @@
+let primaEsecuzioneErrori = true;
+let primaEsecuzioneEventi = true;
 
 function aggiornaErrori() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('contenuto-file-errori').innerHTML = xhr.responseText;
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const div1 = document.getElementById("contenuto-file-errori");
+            if (div1) {
+                div1.innerHTML = xhr.responseText;
+
+                // Scorri in fondo solo alla prima esecuzione
+                if (primaEsecuzioneErrori) {
+                    div1.scrollTop = div1.scrollHeight;
+                    primaEsecuzioneErrori = false;
+                }
+            }
         }
     };
-    xhr.open('GET', '/Progetto/Utility/ULeggiErrori.php', true);
+    xhr.open('GET', '/Progetto/Utility/ULeggiErrori.php?t=' + new Date().getTime(), true); // previene cache
     xhr.send();
-    const div1 = document.getElementById("contenuto-file-errori");
-    div1.scrollTop = div1.scrollHeight;
 }
-setInterval(aggiornaErrori, 1000); // Aggiorna ogni secondo
 
 function aggiornaEventi() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('contenuto-file-eventi').innerHTML = xhr.responseText;
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const div2 = document.getElementById("contenuto-file-eventi");
+            if (div2) {
+                div2.innerHTML = xhr.responseText;
+
+                // Scorri in fondo solo alla prima esecuzione
+                if (primaEsecuzioneEventi) {
+                    div2.scrollTop = div2.scrollHeight;
+                    primaEsecuzioneEventi = false;
+                }
+            }
         }
     };
-    xhr.open('GET', '/Progetto/Utility/ULeggiEventi.php', true);
+    xhr.open('GET', '/Progetto/Utility/ULeggiEventi.php?t=' + new Date().getTime(), true); // previene cache
     xhr.send();
-    const div2 = document.getElementById("contenuto-file-eventi");
-    div2.scrollTop = div2.scrollHeight;
 }
-setInterval(aggiornaEventi, 1000); // Aggiorna ogni secondo
+
+// Esecuzione iniziale immediata
+aggiornaErrori();
+aggiornaEventi();
+
+// Aggiorna ogni secondo
+setInterval(aggiornaErrori, 1000);
+setInterval(aggiornaEventi, 1000);
