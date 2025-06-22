f015bb0ea4ec77f80861e1c32c47c71a064f9f30
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-22 22:16:45 +0200

    Update EArticle.php

--- Progetto/Entity/EArticle.php
+++ Progetto/Entity/EArticle.php
@@ -37,11 +37,11 @@ class EArticle{
     private $reviews = [];
     
     #[ORM\ManyToOne(targetEntity: "EUser", inversedBy: "articles", cascade: ["persist","remove"])]
-    #[ORM\JoinColumn(name: "fk_writer", referencedColumnName: "user_id", nullable: false, onDelete: "cascade")] // definizione chiave esterna
+    #[ORM\JoinColumn(name: "fk_writer", referencedColumnName: "user_id", nullable: false, onDelete: "casade")] // definizione chiave esterna
     // definisco il nome del campo dell'altra tabella che è chiave esterna
     private EUser $writer;
     
-    #[ORM\OneToMany(targetEntity: "EReading", mappedBy: "articleId", cascade: ["persist", "remove"], orphanRemoval: true)]
+    #[ORM\OneToMany(targetEntity: "EReading", mappedBy: "articleId", cascade: ["persist", "remove"])]
     private $readings = [];
 
 
