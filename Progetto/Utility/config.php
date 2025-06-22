600f06feedab688671b9e4ecf147d21db96af81c
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-22 10:48:21 +0200

    Update config.php

--- Progetto/Utility/config.php
+++ Progetto/Utility/config.php
@@ -12,3 +12,9 @@ define('COOKIE_EXP_TIME', 2592000); // 30 days in seconds
 //points per Reading
 define('POINTS', 10);
 define('POINTS_MULTIPLIER', 0.01);
+
+// users
+define('BASIC', 0);
+define('READER', 1);
+define('WRITER', 2);
+define('ADMIN', 3);
