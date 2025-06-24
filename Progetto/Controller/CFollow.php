f9cb751580db1d84e870b09f8a9275e521add7b8
3ndryw <andreacostantini03@gmail.com>
2025-06-24 16:42:34 +0200

    Update CFollow.php

--- Progetto/Controller/CFollow.php
+++ Progetto/Controller/CFollow.php
@@ -19,8 +19,8 @@ class CFollow{
                     $writer = FPersistentManager::getInstance()->retrieveUserOnUsername($usernameWriter);
                     if (isset($writer)){
                         $follow = new EFollow($user, $writer);
-                        $user->addFollowing($follow);
-                        $writer->addFollower($follow);
+                        $user->addFollowing($writer);
+                        $writer->addFollower($user);
                         FPersistentManager::getInstance()->saveInDb($follow);
                         FPersistentManager::getInstance()->saveInDb($user);
                         FPersistentManager::getInstance()->saveInDb($writer);
@@ -59,8 +59,8 @@ class CFollow{
                     $follow = $user->getFollowingById($writer->getId());
                     if (isset($writer) and isset($follow)){
                         $follow = new EFollow($user, $writer);
-                        $user->removeFollowing($follow);
-                        $writer->removeFollower($follow);
+                        $user->removeFollowing($writer);
+                        $writer->removeFollower($user);
                         FPersistentManager::getInstance()->delete($follow);
                         FPersistentManager::getInstance()->saveInDb($user);
                         FPersistentManager::getInstance()->saveInDb($writer);
