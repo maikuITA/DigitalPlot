7fdcd1b75557d6d31c509989880d37c385d917be
3ndryw <andreacostantini03@gmail.com>
2025-06-26 12:45:02 +0200

    Add dashboard statistics for articles and purchases
    Implemented methods in FPersistentManager and FEntityManager to count records and retrieve counts by date. Updated CDashboard to use these methods for displaying article and purchase statistics for different time periods.

--- Progetto/Controller/CDashboard.php
+++ Progetto/Controller/CDashboard.php
@@ -21,9 +21,16 @@ class CDashboard{
      */
     public static function dashboardUpdate(): void{
         if(CUser::isLogged() && CUser::isAdmin()){
-            $
+            $numArtiG = FPersistentManager::getInstance()->retrieveNumOnDate(EArticle::class, 'releaseDate', date('Y-m-d', strtotime('-1 day')));
+            $numArtiS = FPersistentManager::getInstance()->retrieveNumOnDate(EArticle::class, 'releaseDate', date('Y-m-d', strtotime('-1 week')));
+            $numArtiM = FPersistentManager::getInstance()->retrieveNumOnDate(EArticle::class, 'releaseDate', date('Y-m-d', strtotime('-1 month')));
+            $numArtiT = FPersistentManager::getInstance()->countRecord(EArticle::class);
+            $numPurG = FPersistentManager::getInstance()->retrieveNumOnDate(EPurchase::class, 'purchaseDate', date('Y-m-d', strtotime('-1 day')));
+            $numPurS = FPersistentManager::getInstance()->retrieveNumOnDate(EPurchase::class, 'purchaseDate', date('Y-m-d', strtotime('-1 week')));
+            $numPurM = FPersistentManager::getInstance()->retrieveNumOnDate(EPurchase::class, 'purchaseDate', date('Y-m-d', strtotime('-1 month')));
+            $numPurT = FPersistentManager::getInstance()->countRecord(EPurchase::class);
         }else{
-
+            exit;
         }
     }
 }
