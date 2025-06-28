6c89a71ba8cb2dfa0013bf0c3fab4b6501426d7c
3ndryw <andreacostantini03@gmail.com>
2025-06-28 20:22:59 +0200

    Update CPurchase.php

--- Progetto/Controller/CPurchase.php
+++ Progetto/Controller/CPurchase.php
@@ -1,36 +1,37 @@
 <?php
 
-class CPurchase{
+class CPurchase
+{
 
     /**
      * This class launch the purchase form
      * @param int $subscriptionCod The code of the subscription to purchase
      * @return void
-    */
-    public static function startPurchase(int $subscriptionCod = -1): void {
-        if($subscriptionCod === null || $subscriptionCod <= 0){
+     */
+    public static function startPurchase(int $subscriptionCod = -1): void
+    {
+        if ($subscriptionCod === null || $subscriptionCod <= 0) {
             header('Location: https://digitalplot.altervista.org/error/404');
             exit();
         }
         $subscription = FPersistentManager::getInstance()->retrieveObjById(ESubscription::class, $subscriptionCod);
-        if(!isset($subscription)){
-                header('Location: https://digitalplot.altervista.org/error/404');
-                exit();
+        if (!isset($subscription)) {
+            header('Location: https://digitalplot.altervista.org/error/404');
+            exit();
         }
-        if(CUser::isLogged()){
-            if (!CUser::isSubbed()){
+        if (CUser::isLogged()) {
+            if (!CUser::isSubbed()) {
                 $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
                 $points = EPurchase::calculateDiscount($subscription, $user->getPlotCard()->getPoints());
-                VPurchase::startPurchase($user,true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege(), $subscription, $points);
+                VPurchase::startPurchase($user, true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege(), $subscription, $points);
             } else {
                 header('Location: https://digitalplot.altervista.org/home');
                 exit();
             }
-            
-        } else{
+        } else {
             header('Location: https://digitalplot.altervista.org/auth');
             exit();
-        } 
+        }
     }
 
     /**
@@ -38,48 +39,49 @@ class CPurchase{
      * @param int $subscriptionCod The code of the subscription to purchase
      * @return void
      */
-    public static function purchase(int $subscriptionCod = -1): void {
-        if($subscriptionCod === null || $subscriptionCod <= 0){
+    public static function purchase(int $subscriptionCod = -1): void
+    {
+        if ($subscriptionCod === null || $subscriptionCod <= 0) {
             header('Location: https://digitalplot.altervista.org/error/404');
             exit();
         }
-        if(!CUser::isLogged()){
+        if (!CUser::isLogged()) {
             header('Location: https://digitalplot.altervista.org/auth');
             exit();
         }
-        if (!CUser::isSubbed()){
+        if (!CUser::isSubbed()) {
             //retriving all object
             $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
             $subscription = FPersistentManager::getInstance()->retrieveObjById(ESubscription::class, $subscriptionCod);
             $card = self::getCreditCard();
+            $possibleCard = FPersistentManager::getInstance()->retrieveObjById(ECreditCard::class, $card->getCardNumber());
             // verify if credit card already exists in db, if yes, take the card from it
-            if (FPersistentManager::getInstance()->retrieveObjById(ECreditCard::class, $card->getCardNumber()) === false){
+            if (isset($possibleCard)) {
                 FPersistentManager::getInstance()->saveInDb($card);
-            } else{
+            } else {
                 $card = FPersistentManager::getInstance()->retrieveObjById(ECreditCard::class, $card->getCardNumber());
             }
             $purchase =  self::validatePurchase($user, $subscription, $card);
             //calculating discuont and update all object
             $points = EPurchase::calculateDiscount($subscription, $user->getPlotCard()->getPoints());
             $user->getPlotCard()->setPoints($user->getPlotCard()->getPoints() - ($points / POINTS_MULTIPLIER));
-            $user->addPurchase($purchase);   
-            $card->addPurchase($purchase);                      
-            $subscription->addPurchase($purchase); 
+            $user->addPurchase($purchase);
+            $card->addPurchase($purchase);
+            $subscription->addPurchase($purchase);
             //saving in db              
             FPersistentManager::getInstance()->saveInDb($purchase);
             FPersistentManager::getInstance()->saveInDb($subscription);
             FPersistentManager::getInstance()->saveInDb($user);
             //upgrade the user
-            if (strtolower($subscription->getType()) === 'writer' ){
-                FPersistentManager::getInstance()->updateObject(EUser::class, $user->getId(), 'privilege', WRITER); 
-            }else{
+            if (strtolower($subscription->getType()) === 'writer') {
+                FPersistentManager::getInstance()->updateObject(EUser::class, $user->getId(), 'privilege', WRITER);
+            } else {
                 FPersistentManager::getInstance()->updateObject(EUser::class, $user->getId(), 'privilege', READER);
             }
             //showing the view
             header('Location: https://digitalplot.altervista.org/confirm/4');
             exit;
-        }
-        else {
+        } else {
             header('Location: https://digitalplot.altervista.org/home');
             exit();
         }
@@ -90,7 +92,8 @@ class CPurchase{
      * and creates a new ECreditCard object, saving it in the database.
      * @return ECreditCard The created credit card object
      */
-    private static function getCreditCard(): ECreditCard {
+    private static function getCreditCard(): ECreditCard
+    {
         $cardNumber = UHTTPMethods::post('cardNumber');
         $nameC = UHTTPMethods::post('nameC');
         $surnameC = UHTTPMethods::post('surnameC');
@@ -101,7 +104,8 @@ class CPurchase{
     }
 
 
-    private static function validatePurchase(EUser $user, ESubscription $subscription, ECreditCard $card): EPurchase{
+    private static function validatePurchase(EUser $user, ESubscription $subscription, ECreditCard $card): EPurchase
+    {
         $currentdate = date('Y-m-g');
         $country = UHTTPMethods::post('country');
         $city = UHTTPMethods::post('city');
@@ -109,7 +113,7 @@ class CPurchase{
         $zipCode = UHTTPMethods::post('zipCode');
         $billingAddress = UHTTPMethods::post('billingAddress');
         $streetNumber = UHTTPMethods::post('streetNumber');
-        $expirationDate = date('Y-m-d', strtotime("+".$subscription->getPeriod()));
-        return new EPurchase( $currentdate , $expirationDate, $country, $city, $province, $zipCode, $billingAddress, $streetNumber, $user, $subscription, $card );
+        $expirationDate = date('Y-m-d', strtotime("+" . $subscription->getPeriod()));
+        return new EPurchase($currentdate, $expirationDate, $country, $city, $province, $zipCode, $billingAddress, $streetNumber, $user, $subscription, $card);
     }
 }
