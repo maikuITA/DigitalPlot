7628d44329c4b5d2ee7e091062e55b5ab550e1f8
Giulio PORTATILE ACER <86263020+maikuITA@users.noreply.github.com>
2025-06-28 16:12:05 +0200

    Remove console logs and update refresh interval in log.js
    Removed unnecessary console.log statements from log.js for cleaner output. Increased the interval for updating errors and events from 1 second to 10 seconds to reduce resource usage.

--- Progetto/Smarty/js/log.js
+++ Progetto/Smarty/js/log.js
@@ -8,16 +8,13 @@ function buildText(text) {
             testo += riga + '<br>';
         }
     });
-    console.log(testo);
     return testo;
 }
 
 async function aggiornaErrori() {
-    console.log('Aggiorno errori');
     fetch('Progetto/Utility/Logs/errors.log')
         .then((res) => res.text())
         .then((text) => {
-            console.log(text);
             document.getElementById('contenuto-file-errori').innerHTML =
                 buildText(text);
         })
@@ -25,11 +22,9 @@ async function aggiornaErrori() {
 }
 
 async function aggiornaEventi() {
-    console.log('Aggiorno eventi');
     fetch('Progetto/Utility/Logs/events.log')
         .then((res) => res.text())
         .then((text) => {
-            console.log(text);
             document.getElementById('contenuto-file-eventi').innerHTML =
                 buildText(text);
         })
@@ -42,5 +37,5 @@ aggiornaEventi();
 
 // Ogni 5 secondi (meglio di 1 secondo, più sostenibile)
 // 5*60*1000 SONO 5 MINUTI
-setInterval(aggiornaErrori, 1 * 1000); // 10 secondi
-setInterval(aggiornaEventi, 1 * 1000); // 10 secondi
+setInterval(aggiornaErrori, 10 * 1000); // 10 secondi
+setInterval(aggiornaEventi, 10 * 1000); // 10 secondi
