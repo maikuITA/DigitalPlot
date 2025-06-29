648b3e8dba7558e65b8ace1833e75658c432fbc8
3ndryw <andreacostantini03@gmail.com>
2025-06-29 17:01:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,6 @@ require_once 'Progetto/autoload.php';
 require_once 'vendor/autoload.php';
 require_once 'Progetto/Utility/config.php';
 
-// Provo ad istallare il database
-//InstallerDb::install();
-
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-
 // Avvio il runner (FrontController)
 $runner = new CFrontController();
 $runner->start();
-
-?>
