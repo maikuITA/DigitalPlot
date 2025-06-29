c4baa53017684755bee8787685d2d898ecd9fb6f
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-29 17:52:45 +0200

    Update StartSmarty.php

--- Progetto/Install/StartSmarty.php
+++ Progetto/Install/StartSmarty.php
@@ -13,6 +13,13 @@ class StartSmarty
         $smarty->setTemplateDir('./Progetto/Smarty/templates/'); // set the directory for the templates
         $smarty->setCompileDir('./Progetto/Smarty/templates_c/'); // set the directory for the compiled templates
         $smarty->setCacheDir('/membri/digitalplot/Progetto/Smarty/cache/'); // set the directory for the cache
+
+        $smarty->caching = Smarty::CACHING_LIFETIME_CURRENT;  // it turns on caching
+
+        // it sets the cache lifetime to 3600 seconds (1 hour) which is the default value. 
+        // This means that cached templates will be valid for 1 hour before they are regenerated.
+        $smarty->cache_lifetime = 3600;
+
         return $smarty; // return the Smarty object
     }
 }
