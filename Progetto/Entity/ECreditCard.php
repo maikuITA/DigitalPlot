0a1e065675aeba6d0ffecc3e111c3c9f4c3bd7b4
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-30 17:45:33 +0200

    Update ECreditCard.php

--- Progetto/Entity/ECreditCard.php
+++ Progetto/Entity/ECreditCard.php
@@ -6,37 +6,40 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: "CreditCard")]
-class ECreditCard{
-    
+class ECreditCard
+{
+
     #[ORM\Id]
-    #[ORM\Column( name : "card_number" ,type: "string")]
+    #[ORM\Column(name: "card_number", type: "string")]
     public string $cardNumber;
-    
+
     #[ORM\Column(type: "string", length: 100)]
     public string $name;
-    
+
     #[ORM\Column(type: "string", length: 100)]
     public string $surname;
-    
+
     #[ORM\Column(type: "date")]
     public DateTime $expirationDate;
 
     #[ORM\Column(type: "string", length: 3)]
     public string $cvv;
-    
+
     #[ORM\OneToMany(targetEntity: "EPurchase", mappedBy: "creditCardNumber", cascade: ["persist", "remove"])]
     private Collection $purchases;
 
-    public function __construct(string $cardNumber, string $name,string $surname, string $expirationDate, string $cvv) {
+    public function __construct(string $cardNumber, string $name, string $surname, string $expirationDate, string $cvv)
+    {
         $this->cardNumber = $cardNumber;
         $this->name = $name;
         $this->surname = $surname;
         $this->expirationDate = new DateTime($expirationDate);
-        $this->cvv = $cvv; // inizializza il CVV
-        $this->purchases = new ArrayCollection(); // inizializza l'array di acquisti
+        $this->cvv = $cvv;
+        $this->purchases = new ArrayCollection();
     }
 
     // Set methods
+
     public function setCardNumber(string $cardNumber)
     {
         $this->cardNumber = $cardNumber;
@@ -62,48 +65,59 @@ class ECreditCard{
     }
 
     // Get methods
-    public function getCardNumber(): string{
+
+    public function getCardNumber(): string
+    {
         return $this->cardNumber;
     }
 
-    public function getName(): string{
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getSurname(): string{
+    public function getSurname(): string
+    {
         return $this->surname;
     }
 
-    public function getExpirationDate(): DateTime{
+    public function getExpirationDate(): DateTime
+    {
         return $this->expirationDate;
     }
-    public function getCvv(): string{
+    public function getCvv(): string
+    {
         return $this->cvv;
     }
-    
-     //Acquisti
-     public function getPurchases(){
+
+    // Methods for purchases management
+
+    public function getPurchases()
+    {
         return $this->purchases;
     }
-    public function addPurchase(EPurchase $purchase){
-        $this->purchases[] = $purchase;
+    public function addPurchase(EPurchase $purchase)
+    {
+        $this->purchases->add($purchase);
     }
-    public function removePurchase(EPurchase $purchase){
+    public function removePurchase(EPurchase $purchase)
+    {
         $key = $this->purchases->contains($purchase);
         if ($key !== false) {
             $this->purchases->removeElement($purchase);
         }
     }
-    public function purchasesCount(){
-        return count($this->purchases);
+    public function purchasesCount()
+    {
+        return $this->purchases->count();
     }
-    public function getPurchaseById(int $index): ?EPurchase{
-        foreach($this->purchases as $element){
-            if ($element->getId() === $index){
-                return $element;
+    public function getPurchaseById(int $index): ?EPurchase
+    {
+        foreach ($this->purchases as $purchase) {
+            if ($purchase->getId() === $index) {
+                return $purchase;
             }
         }
         return null;
     }
-
 }
