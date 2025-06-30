20a351d0bcf0fcee3a3660e2c614e50bd4bbdf59
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-30 17:18:25 +0200

    Update ESubscription.php

--- Progetto/Entity/ESubscription.php
+++ Progetto/Entity/ESubscription.php
@@ -6,27 +6,31 @@ use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity]
 #[ORM\Table(name: "Subscription")]
-class ESubscription {  // cod type period price
+class ESubscription
+{  // cod type period price
 
     // definisco il campo come chiave primaria con la prima @ORM\Id, mentre la seconda permette di far generare il valore del campo dal sistema (si può specificare la strategia di generazione)
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: "IDENTITY")]
     #[ORM\Column(name: "subscription_id", type: "integer")]
     public int $cod;
-    
+
     #[ORM\Column(type: "string", length: 100)]
-    private string $type; 
-    
+    private string $type;
+
     #[ORM\Column(type: "string", length: 100)]
     private string $period;
-   
+
     #[ORM\Column(type: "float")]
     private float $price;
-    
-    #[ORM\OneToMany(targetEntity: "EPurchase", mappedBy: "subscription", cascade: ["persist"])]  // definisco il nome del campo dell'altra tabella che è chiave esterna
+
+    // there isn't "remove" because if a subscription is removed, all the purchases (not expired) associated with it are still alive and users can use them
+
+    #[ORM\OneToMany(targetEntity: "EPurchase", mappedBy: "subscription", cascade: ["persist"])]
     private Collection $purchases; // array di purchases associati all'abbonamento
-    
-    public function __construct(int $type,string $period, float $price) {
+
+    public function __construct(int $type, string $period, float $price)
+    {
         $this->setType($type);
         $this->period = $period;
         $this->price = $price;
@@ -40,12 +44,11 @@ class ESubscription {  // cod type period price
     }
     public function setType(int $type)
     {
-        if ($type == READER){
+        if ($type == READER) {
             $this->type = "reader";
-        }else{
+        } else {
             $this->type = "writer";
         }
-        
     }
     public function setPeriod(string $period)
     {
@@ -56,40 +59,48 @@ class ESubscription {  // cod type period price
         $this->price = $price;
     }
     // Get methods
-    public function getCod(){
+    public function getCod()
+    {
         return $this->cod;
     }
-    public function getType(){
+    public function getType()
+    {
         return $this->type;
     }
-    public function getPeriod(){
+    public function getPeriod()
+    {
         return $this->period;
     }
-    public function getPrice(){
+    public function getPrice()
+    {
         return $this->price;
     }
     //Acquisti
-    public function getPurchases(): Collection {
+    public function getPurchases(): Collection
+    {
         return $this->purchases;
     }
-    public function addPurchase(EPurchase $purchase): void{
+    public function addPurchase(EPurchase $purchase): void
+    {
         $this->purchases->add($purchase);
     }
-    public function removePurchase(EPurchase $purchase): void{
+    public function removePurchase(EPurchase $purchase): void
+    {
         if ($this->purchases->contains($purchase)) {
             $this->purchases->removeElement($purchase);
         }
     }
-    public function getPurchasesCount(){
+    public function getPurchasesCount()
+    {
         return $this->purchases->count();
     }
-    public function getPurchaseById(int $index){
+    public function getPurchaseById(int $index)
+    {
         foreach ($this->purchases as $purchase) {
             if ($purchase->getCod() === $index) {
                 return $purchase;
             }
         }
-        return null; 
+        return null;
     }
 }
-
