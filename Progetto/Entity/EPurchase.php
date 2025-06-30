28cdc554120b276dab17698875f196fee96a3188
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-30 17:29:11 +0200

    Update EPurchase.php

--- Progetto/Entity/EPurchase.php
+++ Progetto/Entity/EPurchase.php
@@ -4,68 +4,70 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table("Purchase")]
-class EPurchase{
-    
+class EPurchase
+{
+
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: "IDENTITY")]
     #[ORM\Column(name: "id_purchase", type: "integer")]
     private int $cod;
-    
+
     #[ORM\Column(name: "purchase_date", type: "date")]
     private DateTime $purchaseDate;
 
     #[ORM\Column(name: "expire_date", type: "date")]
     private DateTime $expireDate;
-    
+
     //---------------Billing Address----------------
 
-    #[ORM\Column(type:"string", length:40, nullable:false)]
+    #[ORM\Column(type: "string", length: 40, nullable: false)]
     private string $country;
 
-    #[ORM\Column(type:"string", length:40, nullable:false)]
+    #[ORM\Column(type: "string", length: 40, nullable: false)]
     private string $city;
 
-    #[ORM\Column(type:"string", length:40, nullable:false)]
+    #[ORM\Column(type: "string", length: 40, nullable: false)]
     private string $province;
 
-    #[ORM\Column(type:"string", length:5, nullable:false)]
+    #[ORM\Column(type: "string", length: 5, nullable: false)]
     private string $zipCode;
 
-    #[ORM\Column(type:"string", length:40, nullable:false)]
+    #[ORM\Column(type: "string", length: 40, nullable: false)]
     private string $billingAddress;
 
-    #[ORM\Column(type:"string", length:40, nullable:false)]
+    #[ORM\Column(type: "string", length: 40, nullable: false)]
     private string $streetNumber;
 
     //---------------Relationships-------------------
 
     #[ORM\ManyToOne(targetEntity: "EUser", inversedBy: "purchases", cascade: ["persist"])]
-    #[ORM\JoinColumn(name: "fk_subscriber", referencedColumnName: "user_id", nullable: true, onDelete: "cascade")]
+    #[ORM\JoinColumn(name: "fk_subscriber", referencedColumnName: "user_id", nullable: false, onDelete: "cascade")]
     private EUser $subscriber;
 
     #[ORM\ManyToOne(targetEntity: "ESubscription", inversedBy: "purchases", cascade: ["persist"])]
-    #[ORM\JoinColumn(name: "fk_subscription", referencedColumnName: "subscription_id", nullable: false, onDelete: "cascade")] // definizione chiave esterna
+    #[ORM\JoinColumn(name: "fk_subscription", referencedColumnName: "subscription_id", nullable: true, onDelete: "cascade")]
     private ESubscription $subscription;
-     
+
     #[ORM\ManyToOne(targetEntity: "ECreditCard", inversedBy: "purchases", cascade: ["persist"])]
-    #[ORM\JoinColumn(name: "fk_card", referencedColumnName: "card_number", nullable: false, onDelete: "cascade")] // definizione chiave esterna
+    #[ORM\JoinColumn(name: "fk_card", referencedColumnName: "card_number", nullable: false, onDelete: "cascade")]
     private ECreditCard $creditCardNumber;
-     
+
     #[ORM\Column(name: "subtotal", type: "float")]
     private float $subTotal;
 
-
-    public function __construct(string $purchaseDate, 
-                                string $expireDate, 
-                                string $country, 
-                                string $city, 
-                                string $province, 
-                                string $zipCode,
-                                string $billingAddress,
-                                string $streetNumber,
-                                EUser $subscriber, 
-                                ESubscription $subscription, 
-                                ECreditCard $card) {
+    public function __construct(
+        string $purchaseDate,
+        string $expireDate,
+        string $country,
+        string $city,
+        string $province,
+        string $zipCode,
+        string $billingAddress,
+        string $streetNumber,
+        EUser $subscriber,
+        ESubscription $subscription,
+        ECreditCard $card
+    ) {
         $this->purchaseDate = new DateTime($purchaseDate);
         $this->expireDate = new DateTime($expireDate);
         $this->country = $country;
@@ -79,7 +81,7 @@ class EPurchase{
         $this->creditCardNumber = $card;
         $this->subTotal = $subscription->getPrice() - self::calculateDiscount($this->subscription, $subscriber->getPlotCard()->getPoints());
     }
-    
+
     // Set methods
     public function setCod(int $cod)
     {
@@ -92,7 +94,7 @@ class EPurchase{
     public function setExpireDate(string $expireDate)
     {
         $this->expireDate = new DateTime($expireDate);
-    }   
+    }
     public function setCountry(string $country): void
     {
         $this->country = $country;
@@ -117,27 +119,31 @@ class EPurchase{
     {
         $this->streetNumber = $streetNumber;
     }
-    
+
     public function setSubscription(ESubscription $subscription): void
     {
         $this->subscription = $subscription;
     }
-    public function setSubscriber(EUser $subscriber){
+    public function setSubscriber(EUser $subscriber)
+    {
         $this->subscriber = $subscriber;
     }
     public function setCard(ECreditCard $card)
     {
         $this->creditCardNumber = $card;
     }
-    
+
     // Get methods
-    public function getCod(): int{
+    public function getCod(): int
+    {
         return $this->cod;
     }
-    public function getPurchaseDate(): DateTime{
+    public function getPurchaseDate(): DateTime
+    {
         return $this->purchaseDate;
     }
-    public function getExpireDate(): DateTime{
+    public function getExpireDate(): DateTime
+    {
         return $this->expireDate;
     }
     public function getCountry(): string
@@ -172,10 +178,12 @@ class EPurchase{
     {
         return $this->subscriber;
     }
-    public function getCard(): ECreditCard{
+    public function getCard(): ECreditCard
+    {
         return $this->creditCardNumber;
     }
-     public function getSubTotal(): float{
+    public function getSubTotal(): float
+    {
         return $this->subTotal;
     }
 
@@ -185,8 +193,8 @@ class EPurchase{
         $difference = $price - ($points * POINTS_MULTIPLIER);
         $result = $points * POINTS_MULTIPLIER;
         if ($difference > 0) {
-            return $result; 
-        } else{
+            return $result;
+        } else {
             return $price;
         }
     }
