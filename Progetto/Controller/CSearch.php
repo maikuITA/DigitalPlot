95624aabab1d3b59af10f594a639ca321ddb45bc
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-30 19:55:19 +0200

    Update CSearch.php

--- Progetto/Controller/CSearch.php
+++ Progetto/Controller/CSearch.php
@@ -1,20 +1,22 @@
 <?php
-class CSearch {
+class CSearch
+{
 
     /**
      * Method to take value in the search screen
      * @return void
      */
-    public static function takeValueArticle(): void {
+    public static function takeValueArticle(): void
+    {
         // Check if the user is logged in
         if (CUser::isLogged()) {
-            if(CUser::isSubbed()){
+            if (CUser::isSubbed()) {
                 $title = UHTTPMethods::post('title');
                 $category = UHTTPMethods::post('category');
                 $genre = UHTTPMethods::post('genre');
                 $releaseDate = UHTTPMethods::post('releaseDate');
                 CSearch::emptyValues($title, $category, $genre, $releaseDate);
-            }else{
+            } else {
                 header('Location: https://digitalplot.altervista.org/home');
             }
         } else {
@@ -24,45 +26,39 @@ class CSearch {
     }
 
     /**
-     * Method to check if the values are empty
+     * Method to search articles based on the provided parameters
      * @param string $title
      * @param string $type
      * @param string $genre
      * @param string $date
      * @return void
      */
-    public static function emptyValues(string $title, string $category, string $genre, string $releaseDate): void {
+    public static function emptyValues(string $title, string $category, string $genre, string $releaseDate): void
+    {
         $title = trim($title);
         $title = "%" . $title . "%";
         $result = FPersistentManager::getInstance()->searchArticles($title, $category, $genre, $releaseDate);
         $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
-        VSearch::displaySearchResults( $result, true,$user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege());    
+        VSearch::displaySearchResults($result, true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege());
     }
 
     /**
-     * Method to authenticate the user
-     * This method checks if the VUser view exists and calls its auth method.
-     * If the VUser view does not exist, it logs an error message.
-     * This method is used to display the authentication page for users.
+     * Method that veryfies if the user is logged in and subscribed
+     * If so, it allows the user to search for articles
+     * If not, it redirects to the authentication page
      * @return void
      */
-    public static function find(): void {
-        if(CUser::isLogged()) {
+    public static function find(): void
+    {
+        if (CUser::isLogged()) {
             $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
-            if(CUser::isSubbed()){
-                VSearch::find(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(),$user->getPrivilege());
-            }
-            else {
+            if (CUser::isSubbed()) {
+                VSearch::find(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege());
+            } else {
                 header('Location: https://digitalplot.altervista.org/home');
             }
         } else {
             header('Location: https://digitalplot.altervista.org/auth');
         }
     }
-
-
-     
 }
-
-
-?>
