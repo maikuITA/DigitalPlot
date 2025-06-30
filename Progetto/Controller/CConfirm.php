c0c58ee795abc209ea1597b82453af9bff05e3e8
3ndryw <andreacostantini03@gmail.com>
2025-06-30 19:07:17 +0200

    ciao

--- Progetto/Controller/CConfirm.php
+++ Progetto/Controller/CConfirm.php
@@ -2,35 +2,39 @@
 
 use Doctrine\DBAL\Types\Type;
 
-class CConfirm{
+class CConfirm
+{
 
     /**
      * Method to handle confirmation
      * This method will redirect the user to a custom 404 error page.
      * @return void
      */
-    public static function confirm(int $type = 0): void {
-        if(CUser::isLogged()) {
+    public static function confirm(int $type = 0): void
+    {
+        if (CUser::isLogged()) {
             $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
-            if ($type === 1){
-                VConfirm::render(confirmMessage: "L'articolo è stato eliminato correttamente!", plotPoints: $user->getPlotCard()->getPoints(), proPic:$user->getEncodedData(), privilege: $user->getPrivilege(), isLogged:true, type:$type);
-            } elseif ($type === 2){
-                VConfirm::render(confirmMessage:"Articolo salvato correttamente!", plotPoints:$user->getPlotCard()->getPoints(), proPic:$user->getEncodedData(), privilege:$user->getPrivilege(), isLogged: true,type: $type);
-            } elseif ($type === 3){
-                VConfirm::render(confirmMessage:"Commento rimosso con successo", plotPoints:$user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(),isLogged: true,type: $type);
-            } elseif ($type === 4){
-                VConfirm::render( confirmMessage:"Grazie ". $user->getUsername() . " per esserti abbonato!" ,plotPoints: $user->getPlotCard()->getPoints(), proPic:$user->getEncodedData(), privilege:$user->getPrivilege(), isLogged:true,type:$type);
-            } elseif ($type === 5){
-                VConfirm::render(confirmMessage: "Arrivederci!",plotPoints: 0, proPic: null , privilege: 0 ,isLogged:false, type: $type);
-            } elseif ($type === 6){
-                VConfirm::render(confirmMessage: "L'articolo è stato approvato!",plotPoints: 0, proPic: null , privilege: ADMIN ,isLogged: true, type: $type);
-            } elseif ($type === 7){
-                VConfirm::render(confirmMessage: "L'articolo è stato scartato!",plotPoints: 0, proPic: null , privilege: ADMIN ,isLogged: true, type: $type);
-            } elseif ($type === 8){
-                VConfirm::render(confirmMessage: "Il profilo è stato modificato con successo!", plotPoints: 0, proPic: null , privilege: 0 ,isLogged: true, type: $type);
+            if ($type === 1) {
+                VConfirm::render(confirmMessage: "L'articolo è stato eliminato correttamente!", plotPoints: $user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(), isLogged: true, type: $type);
+            } elseif ($type === 2) {
+                VConfirm::render(confirmMessage: "Articolo salvato correttamente!", plotPoints: $user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(), isLogged: true, type: $type);
+            } elseif ($type === 3) {
+                VConfirm::render(confirmMessage: "Commento rimosso con successo", plotPoints: $user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(), isLogged: true, type: $type);
+            } elseif ($type === 4) {
+                VConfirm::render(confirmMessage: "Grazie " . $user->getUsername() . " per esserti abbonato!", plotPoints: $user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(), isLogged: true, type: $type);
+            } elseif ($type === 5) {
+                VConfirm::render(confirmMessage: "Arrivederci!", plotPoints: 0, proPic: null, privilege: 0, isLogged: false, type: $type);
+            } elseif ($type === 6) {
+                VConfirm::render(confirmMessage: "L'articolo è stato approvato!", plotPoints: 0, proPic: null, privilege: ADMIN, isLogged: true, type: $type);
+            } elseif ($type === 7) {
+                VConfirm::render(confirmMessage: "L'articolo è stato scartato!", plotPoints: 0, proPic: null, privilege: ADMIN, isLogged: true, type: $type);
+            } elseif ($type === 8) {
+                VConfirm::render(confirmMessage: "Il profilo è stato modificato con successo!", plotPoints: 0, proPic: null, privilege: 0, isLogged: true, type: $type);
+            } else {
+                VConfirm::render(confirmMessage: "Operazione completata con successo!", plotPoints: $user->getPlotCard()->getPoints(), proPic: $user->getEncodedData(), privilege: $user->getPrivilege(), isLogged: true, type: 4);
             }
         } else {
-            VConfirm::render(confirmMessage: "Arrivederci!", plotPoints:0, proPic: null ,privilege: BASIC,isLogged:false, type: $type);        
+            VConfirm::render(confirmMessage: "Arrivederci!", plotPoints: 0, proPic: null, privilege: BASIC, isLogged: false, type: $type);
         }
     }
 }
