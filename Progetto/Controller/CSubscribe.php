c4dcc6ffe3a8cddc480e09722ec0bada9b1dc449
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-06-30 19:27:00 +0200

    Update CSubscribe.php

--- Progetto/Controller/CSubscribe.php
+++ Progetto/Controller/CSubscribe.php
@@ -1,29 +1,28 @@
 <?php
 
-class CSubscribe {
-    
+class CSubscribe
+{
+
     /**
      * Method to render the subscription view
-     * This method checks if the VSubscribe view file exists and renders it.
-     * If the file does not exist, it logs an error message.
+     * This method checks if user is subbed and if so, redirects to the home page.
+     * If not, it shows VSubscribe.
      * @return void
      */
-    public static function subscribe(): void {
-        if(CUser::isLogged()) {
+    public static function subscribe(): void
+    {
+        if (CUser::isLogged()) {
             $user = FPersistentManager::getInstance()->retrieveObjById(EUser::class, USession::getSessionElement('user'));
             $subs = FPersistentManager::getInstance()->retrieveAllSubscriptions();
-            if(CUser::isSubbed()){
+            if (CUser::isSubbed()) {
                 header('Location: https://digitalplot.altervista.org/home');
                 exit;
-            }
-            else {
+            } else {
                 VSubscribe::render(true, $user->getPlotCard()->getPoints(), $user->getEncodedData(), $user->getPrivilege(), $subs);
             }
         } else {
             header('Location: https://digitalplot.altervista.org/auth');
             exit;
         }
-        
     }
-
 }
