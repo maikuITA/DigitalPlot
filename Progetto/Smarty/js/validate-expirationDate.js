02655273145ef39e9afa4001c5deda9e5b930c90
3ndryw <andreacostantini03@gmail.com>
2025-07-01 19:01:21 +0200

    Update validate-expirationDate.js

--- Progetto/Smarty/js/validate-expirationDate.js
+++ Progetto/Smarty/js/validate-expirationDate.js
@@ -6,27 +6,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     expirationDateInput.addEventListener('change', function () {
         const today = new Date();
-        const selectedDate = new Date(this.value); // Il valore di input[type="month"] è "YYYY-MM"
+        const selectedDate = new Date(this.value);
 
-
+        // set today to the first day of the next month
         today.setMonth(today.getMonth() + 1);
-        today.setDate(1); // Imposta il giorno al primo del mese corrente
-        today.setHours(0, 0, 0, 0); // Azzera l'ora per confronti precisi
+        today.setDate(1);
+        today.setHours(0, 0, 0, 0);
 
-        selectedDate.setHours(0, 0, 0, 0); // Azzera l'ora
+        selectedDate.setHours(0, 0, 0, 0);
 
         if (selectedDate <= today) {
             feedbackExpiry.textContent = 'La carta è scaduta';
             feedbackExpiry.style.color = 'red';
-            // Impedisce l'invio del form se la data non è valida
+            // feedback, it avoids form submission if the date is invalid
             this.setCustomValidity('Data di scadenza non valida.');
         } else {
             feedbackExpiry.textContent = 'Data di scadenza valida.';
             feedbackExpiry.style.color = 'green';
-            this.setCustomValidity(''); // Rimuove il messaggio di errore personalizzato
+            // the error messae in empty, the submit botton works 
+            this.setCustomValidity('');
         }
     });
 
-    // ... il resto del tuo codice JavaScript per la validazione della carta ...
-    // Assicurati che questo codice sia all'interno del listener DOMContentLoaded
+
 });
